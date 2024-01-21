3662cd5102ecd89a73c4fc6adb92fd2e06d39f56
bingual <117990926+bingual@users.noreply.github.com>
2024-01-22 01:22:15 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = set()
+
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i == j:
+                continue
+            answer.add(numbers[i] + numbers[j])
+
+    answer = sorted(answer)
+    return answer
