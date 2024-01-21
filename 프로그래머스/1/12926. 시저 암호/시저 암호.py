a3f7407925eec3f90f29b7eef4f974cd4f80bb81
bingual <117990926+bingual@users.noreply.github.com>
2024-01-21 22:49:10 +0900

    [level 1] Title: 시저 암호, Time: 1.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -3,8 +3,10 @@ def solution(s, n):
 
     for c in s:
         if c.isalpha():
-            combo = ord("a") if c.islower() else ord("A")
-            shift = chr((ord(c) - combo + n) % 26 + combo)
+            # 알파벳 총 개수는 26, 문자가 b이상 n이 25이상 이라면 밀엇을 때 알파벳 유니코드 범위를 벗어 나기에 등차 적용후 밀기
+            standard = ord("a") if c.islower() else ord("A")  # 문자가 소문자, 대문자 인지로 기준 설정
+            # s='a', n=25 -> (97 - 97 + 25) % 26 + 97 = 122 -> 'z'
+            shift = chr((ord(c) - standard + n) % 26 + standard)
             answer.append(shift)
         else:
             answer.append(c)
