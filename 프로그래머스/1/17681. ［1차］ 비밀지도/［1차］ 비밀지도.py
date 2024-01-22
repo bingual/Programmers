09b516e97f68835cd7f51d269dcc02b41b345b23
bingual <117990926+bingual@users.noreply.github.com>
2024-01-22 13:57:11 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,13 +1,19 @@
 def solution(n, arr1, arr2):
+    answer = []
+    # 공백으로 이루어진 전체맵
     maps = [[" " for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
-        m1 = bin(arr1[i]).replace("0b", "", 1).rjust(n, "0")
-        m2 = bin(arr2[i]).replace("0b", "", 1).rjust(n, "0")
+        # 맵을 순회 하면서 십진수 -> 이진수 변환
+        m1 = bin(arr1[i])[2:].rjust(n, "0")
+        m2 = bin(arr2[i])[2:].rjust(n, "0")
 
+        # 벽을 #으로 변환
         for j in range(n):
             if m1[j] == "1" or m2[j] == "1":
                 maps[i][j] = "#"
 
-    answer = ["".join(a) for a in maps]
+        # 출력 결과에 맞게 삽입
+        answer.append("".join(maps[i]))
+
     return answer
