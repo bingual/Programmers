5d80f9c85dd2b37ed09a26b5e04baec12ed7ec5f
bingual <117990926+bingual@users.noreply.github.com>
2024-01-23 01:32:46 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,3 +1,16 @@
 def solution(k, m, score):
     answer = 0
+    score.sort(reverse=True)  # 사과 상자 정렬
+
+    # 사과 상자를 각각 m 만큼 잘라서 최대 이익의 조합 생성. 남는 사과는 버림
+    comb = []
+    for i in range(0, len(score), m):
+        temp = score[i : m + i]
+        if len(temp) == m:
+            comb.append(temp)
+
+    # 총 판매 수익 계산
+    for a in comb:
+        answer += a[-1] * m
+
     return answer
