1ef3ebbe16aaadec7303fbc46344d361df0188e3
bingual <117990926+bingual@users.noreply.github.com>
2024-01-23 16:20:13 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -4,12 +4,12 @@ from itertools import combinations
 def solution(nums):
     answer = 0
     for item in combinations(nums, 3):
-        divisors = []
         sum_comb = sum(item)
-        for i in range(1, sum_comb + 1):
+        for i in range(2, sum_comb):
             if sum_comb % i == 0:
-                divisors.append(i)
-        if len(divisors) == 2:
+                break
+        else:
+            # 1과 자신을 제외하고 나누어 떨어지면 소수가 아니니 카운트
             answer += 1
 
     return answer
