b6bfd6f0dadbc4589575c40cfbb60081cc16c0a4
bingual <117990926+bingual@users.noreply.github.com>
2024-01-27 23:45:01 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,13 +1,16 @@
 def solution(board, moves):
     answer = 0
     stack = []
+
     for move in moves:
         for line in board:
+            # 인형이 존재 한다면 인형을 뽑고 방문 처리
             if line[move - 1] != 0:
                 stack.append(line[move - 1])
                 line[move - 1] = 0
                 break
 
+        # 같은 인형이 뽑혔을 때 해당수 만큼 바구니를 비우고 카운트
         if len(stack) >= 2 and stack[-1] == stack[-2]:
             del stack[-1]
             del stack[-1]
