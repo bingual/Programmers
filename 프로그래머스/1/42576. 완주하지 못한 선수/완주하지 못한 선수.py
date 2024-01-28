66ad4180f0331e501282641595ce1d5b675574e2
bingual <117990926+bingual@users.noreply.github.com>
2024-01-29 03:31:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.25 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -3,12 +3,11 @@ from collections import Counter
 
 def solution(participant, completion):
     answer = ""
-    # 각 선수의 이름 해쉬화
     p_counter = Counter(participant)
     c_counter = Counter(completion)
-    # 해당 선수의 이름이 존재 하지 않다면 선수 이름 대입
-    for key, value in p_counter.items():
-        if value != c_counter[key]:
-            answer = key
+
+    for player in participant:
+        if c_counter[player] != p_counter[player]:
+            answer = player
 
     return answer
