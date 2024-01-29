de5b6bf15417e9c51eaa0667d49fdfd7eb60546a
bingual <117990926+bingual@users.noreply.github.com>
2024-01-30 02:31:01 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889#) 
 
 ### 성능 요약
 
-메모리: 10.7 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 2:52:10
+2024년 1월 2일 2:30:57
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,33 +1,22 @@
-from fractions import Fraction
-from collections import defaultdict
+from collections import Counter
 from functools import cmp_to_key
 
 
 def solution(N, stages):
-    mol = defaultdict(int)
-    den = {}
-
-    # 분자 구하기
-    for i, item in enumerate(stages):
-        mol[item] += 1
-
-    # 분모 구하기
-    temp = len(stages)
-    for i in range(1, N + 1):
-        temp -= mol[i]
-        den[i] = temp
+    stage = Counter(stages)  # 분자
+    m = len(stages)  # 분모
 
     # 실패율 구하기
     failure = {}
     for i in range(1, N + 1):
-        # 한 스테이지에서 전원 실패일 경우 분모가 0이면 0으로 나눌 수 가 없기에 분자를 값으로 넣어줌
-        if den[i] != 0:
-            failure[i] = Fraction(mol[i], den[i])
+        m -= stage[i - 1]
+        if m != 0:
+            failure[i] = stage[i] / m
         else:
-            failure[i] = mol[i]
+            failure[i] = 0
 
     # 실패율 기준 내림차 정렬
-    sorted_failure = sorted(failure.items(), key=cmp_to_key(cmp), reverse=True)
+    sorted_failure = sorted(failure.items(), key=cmp_to_key(cmp))
 
     # 출력 결과에 맞게 변환
     answer = [item[0] for item in sorted_failure]
@@ -35,4 +24,4 @@ def solution(N, stages):
 
 
 def cmp(x, y):
-    return x[1] - y[1]
+    return y[1] - x[1]
