70e9cab8a9aa9f08fcd16e2aab8134d530356b98
bingual <117990926+bingual@users.noreply.github.com>
2024-02-05 00:34:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -5,23 +5,17 @@ def solution(n, words):
     unique = set()
     temp = ""
     order = 1
+
     for i, word in enumerate(words):
         cnt[order] += 1
 
-        # 중복된 단어 탈락 처리
-        if word in unique:
+        # 중복된 단어 포함, 끝말잇기가 아니면 탈락 처리
+        if word in unique or i != 0 and temp != word[0]:
             answer = [order, cnt[order]]
             break
-        # 끝말잇기가 아니면 탈락 처리
-        if i != 0 and temp != word[0]:
-            answer = [order, cnt[order]]
-            break
-        # 순서 초기화
-        if order == n:
-            order -= order
 
         unique.add(word)
         temp = word[-1]
-        order += 1
+        order = (order % n) + 1
 
     return answer
