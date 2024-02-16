a4a3a0efeda07eb223852dc29a7ea494a76d9c2c
bingual <117990926+bingual@users.noreply.github.com>
2024-02-16 16:03:48 +0900

    [level 2] Title: 더 맵게, Time: 1731.19 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -5,13 +5,11 @@ def solution(scoville, K):
     answer = 0
 
     heapq.heapify(scoville)
-    while scoville[0] < K:
-        if len(scoville) < 2:
-            return -1
+    while len(scoville) > 1 and scoville[0] < K:
         min1 = heapq.heappop(scoville)
         min2 = heapq.heappop(scoville)
-        cal = min1 + (min2 * 2)
+        cal = min1 + min2 * 2
         heapq.heappush(scoville, cal)
         answer += 1
 
-    return answer
+    return -1 if scoville[0] < K else answer
