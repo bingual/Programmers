d594c3cd1db66a52430fe9b76dd3af8f7508ce59
bingual <117990926+bingual@users.noreply.github.com>
2024-02-20 17:58:34 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.33 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,25 +1,6 @@
+import re
+
+
 def solution(files):
-    hnt = []
-    for i, file in enumerate(files):
-        state = [False] * 2
-        h, n, t = "", "", ""
-        for char in file:
-            if not char.isdigit() and not state[0]:
-                h += char
-            if char.isdigit() and not state[1]:
-                state[0] = True
-                n += char
-            elif state[0]:
-                state[1] = True
-                t += char
-        hnt.append((h, n, t))
-
-    sort = sorted(
-        hnt,
-        key=lambda x: (
-            x[0].lower(),
-            int(x[1]),
-        ),
-    )
-
-    return ["".join([h, n, t]) for h, n, t in sort]
+    d = re.compile(r"\d+")
+    return sorted(files, key=lambda x: (re.split(d, x)[0].lower(), int(re.findall(d, x)[0])))
