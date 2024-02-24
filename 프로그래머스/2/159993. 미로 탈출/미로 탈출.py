1406972b4a74d675bc68912aca7cbbee3f6cca84
bingual <117990926+bingual@users.noreply.github.com>
2024-02-24 12:50:28 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -2,13 +2,15 @@ from collections import deque
 
 
 def solution(maps):
-    visited = [list(item[:]) for item in maps]  # 방문 경로를 저장할 변수 선언
-    n, m = len(visited), len(visited[0])
+    answer = 0
 
-    def bfs(y, x, key, graph):
+    n, m = len(maps), len(maps[0])
+    visited = [[char for char in word] for word in maps]
+
+    def bfs(y, x, v, t):
         dy = [-1, 1, 0, 0]
         dx = [0, 0, -1, 1]
-        graph[y][x] = "X"
+        visited[y][x] = "X"
 
         que = deque()
         que.append((y, x, 0))
@@ -16,35 +18,31 @@ def solution(maps):
         while que:
             y, x, c = que.popleft()
 
-            # 찾고자 하는 목표에 도달 했다면 탐색에 걸린 시간 반환
-            if graph[y][x] == key:
+            if v[y][x] == t:
                 return c
 
             for i in range(4):
-                ny, nx = y + dy[i], x + dx[i]
-                if 0 <= ny < n and 0 <= nx < m and graph[ny][nx] != "X":
-                    # 목표가 아닐 경우 방문 처리
-                    if graph[ny][nx] != key:
-                        graph[ny][nx] = "X"
+                ny = y + dy[i]
+                nx = x + dx[i]
+
+                if 0 <= ny < n and 0 <= nx < m and v[ny][nx] != "X":
+                    if v[ny][nx] != t:
+                        v[ny][nx] = "X"
                     que.append((ny, nx, c + 1))
         return -1
 
-    # 시작 위치 -> 레버 위치 탐색
     result1 = 0
     for i in range(n):
         for j in range(m):
             if visited[i][j] == "S":
-                result1 = bfs(i, j, "L", visited)
+                result1 = bfs(i, j, visited, "L")
 
-    visited = [list(item[:]) for item in maps]  # 방문 경로 초기화
+    visited = [[char for char in word] for word in maps]
 
-    # 레버 위치 -> 출구 위치 탐색
     result2 = 0
     for i in range(n):
         for j in range(m):
             if visited[i][j] == "L":
-                result2 = bfs(i, j, "E", visited)
+                result2 = bfs(i, j, visited, "E")
 
-    # 출구나 레버에 도달할 수 없을 경우 -1 반환, 도달할 수 있다면 탐색에 걸린 시간을 합산후 반환
-    answer = -1 if result1 == -1 or result2 == -1 else result1 + result2
-    return answer
+    return -1 if result1 == -1 or result2 == -1 else result1 + result2
