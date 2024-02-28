89c5ef4c27ab3f7fc162057d2ffbd0a04b6c8097
bingual <117990926+bingual@users.noreply.github.com>
2024-02-28 15:16:27 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 45.88 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -3,33 +3,34 @@ from collections import deque
 
 def solution(queue1, queue2):
     answer = 0
-    
+
     a = deque(queue1)
     b = deque(queue2)
-    cnt, a_sum, b_sum = 0, sum(a), sum(b)
+
+    a_sum, b_sum = sum(a), sum(b)
     total = a_sum + b_sum
     i_sum = total // 2
-    
+
     if total % 2 != 0:
         return -1
-    
+
     maxsize = max(len(a), len(b)) * 3
-    while a_sum != i_sum and b_sum != i_sum and a and b:
+    while a_sum != i_sum and b_sum != i_sum:
         if maxsize <= answer:
             return -1
-        
+
         if a_sum > b_sum:
             p = a.popleft()
             b.append(p)
             a_sum -= p
             b_sum += p
             answer += 1
-            
+
         if b_sum > a_sum:
             p = b.popleft()
             a.append(p)
             b_sum -= p
             a_sum += p
             answer += 1
-    
+
     return -1 if not a or not b else answer
