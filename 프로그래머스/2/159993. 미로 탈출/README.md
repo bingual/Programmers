a897e69547a29c87857a0ed5cf27a7d58529d2ef
bingual <117990926+bingual@users.noreply.github.com>
2024-03-12 01:44:36 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.05 ms
+메모리: 10.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 24일 12:50:20
+2024년 03월 12일 01:44:30
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,48 +1,44 @@
 from collections import deque
 
 
+
 def solution(maps):
-    answer = 0
-
-    n, m = len(maps), len(maps[0])
-    visited = [[char for char in word] for word in maps]
-
-    def bfs(y, x, v, t):
-        dy = [-1, 1, 0, 0]
-        dx = [0, 0, -1, 1]
-        visited[y][x] = "X"
-
+    graph = [list(x) for x in maps]
+    n, m = len(graph), len(graph[0])
+    def bfs(x, y, end):
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        graph[x][y] = "X"
+        
         que = deque()
-        que.append((y, x, 0))
-
+        que.append((x, y, 0))
         while que:
-            y, x, c = que.popleft()
-
-            if v[y][x] == t:
+            x, y, c = que.popleft()
+            if graph[x][y] == end:
                 return c
-
+            
             for i in range(4):
-                ny = y + dy[i]
                 nx = x + dx[i]
-
-                if 0 <= ny < n and 0 <= nx < m and v[ny][nx] != "X":
-                    if v[ny][nx] != t:
-                        v[ny][nx] = "X"
-                    que.append((ny, nx, c + 1))
-        return -1
-
-    result1 = 0
+                ny = y + dy[i]
+                
+                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != "X":
+                    if graph[nx][ny] != end:
+                        graph[nx][ny] = "X"
+                    que.append((nx, ny, c + 1))
+        return -1            
+    
+    result1, result2, = 0, 0
     for i in range(n):
         for j in range(m):
-            if visited[i][j] == "S":
-                result1 = bfs(i, j, visited, "L")
-
-    visited = [[char for char in word] for word in maps]
-
-    result2 = 0
+            if graph[i][j] == "S":
+                result1 = bfs(i, j, "L")
+                break
+                
+    graph = [list(x) for x in maps]
     for i in range(n):
         for j in range(m):
-            if visited[i][j] == "L":
-                result2 = bfs(i, j, visited, "E")
-
+            if graph[i][j] == "L":
+                result2 = bfs(i, j, "E")
+                break
+    
     return -1 if result1 == -1 or result2 == -1 else result1 + result2
