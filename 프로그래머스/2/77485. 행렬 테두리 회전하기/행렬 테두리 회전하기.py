11524234f04d74adf49ce0bc2db9e7601e8f0aea
bingual <117990926+bingual@users.noreply.github.com>
2024-03-18 01:38:26 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 164.57 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -3,25 +3,24 @@ def solution(rows, columns, queries):
     board = [[(j + 1) + (i * columns) for j in range(columns)] for i in range(rows)]
     for x1, y1, x2, y2 in queries:
         x1, y1, x2, y2 = x1 - 1, y1 - 1, x2 - 1, y2 - 1
-        copy_board = [row[:] for row in board]
-        min_values = []
-        # top
-        board[x1][y1] = copy_board[x1 + 1][y1]
-        min_values.append(board[x1][y1])
-        for i in range(y1 + 1, y2 + 1):
-            board[x1][i] = copy_board[x1][i - 1]
-            min_values.append(board[x1][i])
-        # right
-        for i in range(x1 + 1, x2 + 1):
-            board[i][y2] = copy_board[i - 1][y2]
-            min_values.append(board[i][y2])
-        # bottom
+        min_value = float("inf")
+        temp = board[x1][y1]
+        min_value = min(min_value, temp)
+        for i in range(x1, x2):
+            board[i][y1] = board[i + 1][y1]
+            min_value = min(min_value, board[i][y1])
+            
         for i in range(y1, y2):
-            board[x2][i] = copy_board[x2][i + 1]
-            min_values.append(board[x2][i])
-        # left
-        for i in range(x1 + 1, x2):
-            board[i][y1] = copy_board[i + 1][y1]
-            min_values.append(board[i][y1])
-        answer.append(min(min_values))
+            board[x2][i] = board[x2][i + 1]
+            min_value = min(min_value, board[x2][i])
+            
+        for i in range(x2, x1, -1):
+            board[i][y2] = board[i - 1][y2]
+            min_value = min(min_value, board[i][y2])
+            
+        for i in range(y2, y1, -1):
+            board[x1][i] = board[x1][i - 1]
+            min_value = min(min_value, board[x1][i])
+        board[x1][y1 + 1] = temp
+        answer.append(min_value)
     return answer
