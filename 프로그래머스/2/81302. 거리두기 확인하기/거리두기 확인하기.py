bf1cece42de4bd2af486f04f7514bcbba52930df
bingual <117990926+bingual@users.noreply.github.com>
2024-03-19 04:11:14 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,7 +1,6 @@
 def solution(places):
     n = len(places[0])
     answer = [1] * n
-
     for idx, place in enumerate(places):
         out = False
         for i in range(n):
@@ -13,13 +12,12 @@ def solution(places):
                         break
             if out:
                 break
-
     return answer
 
 
 def is_valid(place, point):
     n, x, y = point
-    for i in range(n):
+    for i in range(x, n):
         for j in range(n):
             if x == i and y == j:
                 continue
@@ -27,7 +25,7 @@ def is_valid(place, point):
                 dist = abs(x - i) + abs(y - j)
                 if dist < 2:
                     return False
-                if dist <= 2:
+                if dist == 2:
                     for k in range(min(x, i), max(x, i) + 1):
                         for l in range(min(y, j), max(y, j) + 1):
                             if x == k and y == l or i == k and j == l:
