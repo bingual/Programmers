bc56339216e72362446bb2bf07b6a28257c7664c
bingual <117990926+bingual@users.noreply.github.com>
2024-03-22 00:00:49 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.86 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def solution(board):
+    answer = 0
+    n, m = len(board), len(board[0])
+    board = [list(word) for word in board]
+
+    def bfs(x, y):
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        board[x][y] = "V"
+
+        que = deque()
+        que.append((x, y, 0))
+        while que:
+            x, y, c = que.popleft()
+            if board[x][y] == "G":
+                return c
+
+            for i in range(4):
+                tx, ty = x, y
+                while True:
+                    nx, ny = tx + dx[i], ty + dy[i]
+                    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] != "D":
+                        tx, ty = nx, ny
+                    else:
+                        if board[tx][ty] != "V":
+                            que.append((tx, ty, c + 1))
+
+                        if board[tx][ty] != "G":
+                            board[tx][ty] = "V"
+                        break
+        return -1
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == "R":
+                answer = bfs(i, j)
+    return answer
