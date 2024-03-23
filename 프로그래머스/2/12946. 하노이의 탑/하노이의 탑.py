622f09f78f28243962f687b371630e2540012396
bingual <117990926+bingual@users.noreply.github.com>
2024-03-23 14:33:14 +0900

    [level 2] Title: 하노이의 탑, Time: 8.96 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -2,15 +2,11 @@ def solution(n):
     return dfs(n, 1, 3, 2, [])
 
 
-def move(start, to, history):
-    return history.append([start, to])
-
-
 def dfs(n, start, to, mid, history):
     if n == 1:
-        return move(start, to, history)
+        return history.append([start, to])
     else:
         dfs(n - 1, start, mid, to, history)
-        move(start, to, history)
+        history.append([start, to])
         dfs(n - 1, mid, to, start, history)
     return history
