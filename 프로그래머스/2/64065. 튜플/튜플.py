fef4892da158afcb5b6acd71db8be9aa1c38923d
bingual <117990926+bingual@users.noreply.github.com>
2024-04-11 23:05:14 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,15 +1,6 @@
 from collections import Counter
-from functools import cmp_to_key
 import re
 
-
 def solution(s):
-    items = re.sub(r"[^0-9,]+", "", s).split(",")
-    sorted_items = sorted(Counter(items).items(), key=cmp_to_key(cmp))
-    answer = [int(item[0]) for item in sorted_items]
-
-    return answer
-
-
-def cmp(x, y):
-    return y[1] - x[1]
+    s = Counter(map(int, re.sub(f"[^0-9,]", "", s).split(",")))
+    return [x for x, y in sorted(s.items(), key=lambda x: -x[1])]
